2a23f865a9315cdafbd34310cae4bf1505e443ed
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-03-17 17:41:05 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the latters of the alphabet
+ *
+ * Description: print the latters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return(0);
+}
