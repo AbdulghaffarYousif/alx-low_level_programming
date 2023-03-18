eb3e720a19400fa7854ea9f7fbbea9f9af9a88d2
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-03-17 18:26:48 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * eturn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	hile (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
