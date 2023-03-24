71ef46c0755e49e6f7aa23bbd8eafc92b51456b7
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-03-23 17:30:38 -0700

    first commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,9 @@
 
 void print_most_numbers(void)
 {
-	for (int num = '0'; num <= '9' && num != '2' && num != '4'; num++)
+	int num;
+
+	for (num = '0'; num <= '9' && num != '2' && num != '4'; num++)
 	{
 		_putchar(num);
 	}
