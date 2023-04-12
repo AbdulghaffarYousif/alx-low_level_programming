5be0ef3d347f314e2ef92c0950ae09ff8f69a995
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-04-11 19:16:17 -0700

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * creat arry - arry of chars
+ * initiilizes with specific char
+ * @c: char
+ * @size: the size of the memory to print
+ * return: pointing to the array initialized or null
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *num = malloc(size);
+
+	if (size == 0 || num == 0)
+		return (0);
+
+	while (size--)
+		num[size] = c;
+}
