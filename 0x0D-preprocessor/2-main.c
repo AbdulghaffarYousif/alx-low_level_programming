3daae753f669196610a719055a9f01e761a78fbb
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-04-17 17:51:17 -0700

    first commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,11 @@
 /**
  * main - prints the name of the file it was compiled from
  *
- * Return : always 0
+ * Return: always 0
  */
 
-int main (void)
+int main(0)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
