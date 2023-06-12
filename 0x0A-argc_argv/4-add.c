2a11cc5b97a2ce7e033c5f3bb803b1d622581fe4
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-06-12 06:59:54 -0700

    first commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+/**
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (count = 1; count < argc; count++)
+	{
+		for (i = 0; argv[count][i] != '\0'; i++)
+		{
+			if (argv[count][i] > 57 || argv[count][i] < 48)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[count]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
