a075de7bcd7c5662d6d2f51503ec654c53196c7b
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-06-19 07:29:15 -0700

    first commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the name of the file
- * eturn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
