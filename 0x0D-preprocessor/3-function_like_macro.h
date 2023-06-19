865e8da9a5d9d1d2b9e26a902ed0cf1999fc3a84
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-06-19 09:00:55 -0700

    first commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,9 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) if (x > 0)\
-	return (x)\
+#define ABS(x) if ((x) > (0))\
+	return ((x))\
 	else \
-	return (-x)
+	return ((-x))
 
 #endif
