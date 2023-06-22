300da6f2d512def167a9150cd7ab2a9339c77ede
Abdulghaffar <abdulghaffar97civil@gmail.com>
2023-06-22 12:36:02 -0700

    first commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,15 +8,15 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
-
+	unsigned int i;
+	int sum = 0;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		int sum = sum + va_arg(args, int);
+		sum += va_arg(args, int);
 	}
 
 	va_end(args);
