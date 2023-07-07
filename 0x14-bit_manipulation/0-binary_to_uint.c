5bd07e9f7e59440f1ed40ef7829bca29cf7afc43
AbdulghaffarYousif <abdulghaffar97civil@gmail.com>
2023-07-06 19:46:00 -0700

    first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -25,7 +25,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		sum += (b[i] - '0') * power(2, len - i - 1);
+		sum += (b[i] - '0') * pow(2, len - i - 1);
 	}
 	return (sum);
 }
